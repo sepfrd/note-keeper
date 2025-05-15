84b58684238198640e69618b2c1f028ae6ec8651
Sepehr Frd <sepfrd@outlook.com>
2025-05-15 16:25:36 +0330

    feat: add login form component

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        icon: true,
+      },
+    }),
+  ],
+  server: {
+    port: 3000,
+  },
+});
